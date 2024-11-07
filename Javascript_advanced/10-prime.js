437c29e1d670be1e2f1d0e289305e5aced2624e9
josephine324 <j.mutesi@alustudent.com>
2024-11-07 19:14:34 +0200

    javascript advanced

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,33 @@
+function countPrimeNumbers() {
+    const primes = [];
+  
+    for (let i = 2; i <= 100; i++) {
+      let isPrime = true;
+      let end = Math.sqrt(i);
+  
+      for (let j = 2; j <= end; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        primes.push(i);
+      }
+    }
+    return primes;
+  }
+  
+  const t0 = performance.now();
+  
+  for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+  }
+  
+  const t1 = performance.now();
+  
+  console.log(
+    `Execution time of calculating prime numbers 100 times was ${
+      t1 - t0
+    } milliseconds.`
+  );
